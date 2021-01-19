2b15d80f1acc315f2345184dc729e50d9d404f20
ruben forner <9657174+kingdevil731@users.noreply.github.com>
2021-01-19 08:17:56 +0200

    readme and click fix

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 const baseUrl = "https://api.edamam.com/search";
-const apiId = "";
-const apiKey = "";
+const apiId = "app_id=00b90847";
+const apiKey = "app_key=0b8f23d9bccff91819b96789a03741e3";
+
 // paths
 /*
     query = Search Query
