73926868d900039ea476bec74620b9698b60aff3
ruben forner <9657174+kingdevil731@users.noreply.github.com>
2021-01-30 21:42:34 +0200

    Update App.js
    Refactor App so to remove unecessary code as we implemented Redux for state managment

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import Results from "./Components/Results";
 import Modal from "./Components/Modal";
 
 function App() {
-  const [searchQuery, setSearchQuery] = useState("");
-  const [searchResults, setSearchResults] = useState(null);
   const [modalUp, setModalUp] = useState(false);
   const [modalData, setModalData] = useState("");
+  //const [configs, setConfigs] = useState(localStorage.getItem("configs"));
+  const [loaded, setLoaded] = useState(false);
 
   const showModalHandler = (data) => {
     setModalData(data);
@@ -17,24 +17,16 @@ function App() {
 
   return (
     <div className="App">
-      <Nav
-        searchQuery={searchQuery}
-        setSearchQuery={setSearchQuery}
-        searchResults={searchResults}
-        setSearchResults={setSearchResults}
-      />
-      <div className="results-div">
-        {searchResults ? (
+      <Nav loaded={loaded} setLoaded={setLoaded} />
+      {loaded && (
+        <div className="results-div">
           <Results
-            searchResults={searchResults}
             modalUp={modalUp}
             setModalUp={setModalUp}
             showModalHandler={showModalHandler}
           />
-        ) : (
-          ""
-        )}
-      </div>
+        </div>
+      )}
       {modalUp ? (
         <Modal
           modalUp={modalUp}
